15b2f9f7df983d4d97720c7d8d183b1c2730e64d
paologalligit <paolo>
2018-08-11 14:06:42 +0200

    C implementation of reverting a list in place

--- Problem1/ReverseLinkedList.c
+++ Problem1/ReverseLinkedList.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 struct node {
     int value;
@@ -9,14 +10,75 @@ struct node {
 typedef struct node Node;
 typedef Node *NodePtr;
 
-void reverse_in_place(NodePtr head) {
-    NodePtr old = head;
-    NodePtr start = old->next;
+void reverse_in_place(NodePtr *head) {
+    //checking whether head or next is not null
+    if(*head && (*head)->next) {
+        NodePtr old = *head;
+        NodePtr start = old->next;
+        NodePtr tmp = NULL;
 
-    //while(start->)
+        //this will be the last node
+        old->next = NULL;
+        while(start->next) {
+            tmp = start->next;
+            start->next = old;
+            old = start;
+            start = tmp;
+        }
 
+        //changing the new first node pointer
+        start->next = old;
+        *head = start;
+    }
+}
+
+void print_list(NodePtr r) {
+    while(r) {
+        printf("%d -> ", r->value);
+        r = r->next;
+    }
+    puts("NULL");
 }
 
 int main(void) {
+    //test
+
+    //a null list
+    NodePtr null = NULL;
+    reverse_in_place(&null);
+    assert(null == NULL);
+
+    //a list with only one element
+    NodePtr a = malloc(sizeof(Node));
+    a->value = 1;
+    a->next = NULL;
+    print_list(a);
+
+    //a list with 6 elements
+    NodePtr b = malloc(sizeof(Node));
+    NodePtr c = malloc(sizeof(Node));
+    NodePtr d = malloc(sizeof(Node));
+    NodePtr e = malloc(sizeof(Node));
+    NodePtr f = malloc(sizeof(Node));
+
+    b->value = 2;
+    c->value = 3;
+    d->value = 4;
+    e->value = 5;
+    f->value = 6;
+
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+    e->next = f;
+    f->next = NULL;
+
+    print_list(a);
+
+    reverse_in_place(&a);
+
+    print_list(a);
+
     return 0;
 }
